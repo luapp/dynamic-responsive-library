9fb44ae9651f5695dfb5f89a0390eb55716dac74
Paul Le Gall <darky.dev@outlook.com>
2024-02-11 23:54:06 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import AppCss from './App.module.css';
+
+function App() {
+  	return (
+		<div className={AppCss.App}>
+			<h3>dynamic-responsive-library</h3>
+		</div>
+  	)
+}
+
+export default App;
