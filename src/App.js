2c5e15acb86c10fbe157cbaa103bf4acd1dd3f2c
Paul Le Gall <darky.dev@outlook.com>
2024-02-12 13:02:42 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useEffect } from 'react';
 import AppCss from './App.module.css';
 
 function App() {
+
+	useEffect(() => {
+		const handleResize = () => {
+			console.log("--------------------------------------------------------------");
+			console.log('window.innerWidth:', window.innerWidth);
+			console.log('window.innerHeight:', window.innerHeight);
+			console.log('window.devicePixelRatio:', window.devicePixelRatio);
+			//detect zoom level of the browser
+			let zoomLevel = window.devicePixelRatio;
+			console.log('zoomLevel:', zoomLevel);
+			console.log("--------------------------------------------------------------");
+		}
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}
+	, [])
+
   	return (
 		<div className={AppCss.App}>
-			<h3>dynamic-responsive-library</h3>
+			<h2 className={AppCss.NoMargin}>Static H2</h2>
+			<h2 className={AppCss.NoMargin}>Dynamic H2</h2>
+			<div className={AppCss.div}></div>
 		</div>
   	)
 }
